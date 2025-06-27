4634c8d757d4ab0daef2940b0c0413058f8b3c11
Shushant Rishav <54536673+shushantrishav@users.noreply.github.com>
2025-06-27 20:38:28 +0530

    Time: 0 ms (100%), Space: 18.3 MB (77.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,32 +4,30 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         col0 = 1
-        rows = len(matrix)
-        cols = len(matrix[0])
+        n = len(matrix)
+        m = len(matrix[0])
         # First pass — mark rows and columns   
-        for row in range(rows):
-            for col in range(cols):
-                if (matrix[row][col] == 0):
-                    matrix[row][0] = 0
-                    if col != 0:
-                        matrix[0][col] = 0
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    if j != 0:
+                        matrix[0][j] = 0
                     else:
                         col0 = 0
-                    
         # Second pass — set zeroes based on markers (from 1,1)     
-        for row in range(1,rows):
-            for col in range(1,cols):
-                if (matrix[row][0] == 0 and matrix[0][col] == 0):
-                    matrix[row][col] = 0
-        
+        for i in range(1, n):
+            for j in range(1, m):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
         # Handle first row separately if needed
         if matrix[0][0] == 0:
-            for col in range(cols):
-                matrix[0][col] = 0
+            for j in range(m):
+                matrix[0][j] = 0
 
         # Handle first column separately if needed
         if col0 == 0:
-            for row in range(rows):
-                matrix[row][0] = 0
+            for i in range(n):
+                matrix[i][0] = 0
             
         
