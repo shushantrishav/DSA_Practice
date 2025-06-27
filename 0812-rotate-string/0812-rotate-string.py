1068b67ccf0fd96e1a7c2075cc607688f37e958b
Shushant Rishav <54536673+shushantrishav@users.noreply.github.com>
2025-06-27 12:30:56 +0530

    Time: 0 ms (100%), Space: 18 MB (9.67%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -1,3 +1,26 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        return len(s) == len(goal) and goal in (s + s)
+        if len(s) != len(goal):
+            return False
+
+        def compute_z(string):
+            n = len(string)
+            z = [0] * n
+            l, r = 0, 0
+
+            for i in range(1, n):
+                if i <= r:
+                    z[i] = min(r - i + 1, z[i - l])
+
+                while i + z[i] < n and string[z[i]] == string[i + z[i]]:
+                    z[i] += 1
+
+                if i + z[i] - 1 > r:
+                    l, r = i, i + z[i] - 1
+
+            return z
+
+        concat = goal + "$" + s + s
+        z = compute_z(concat)
+
+        return any(val == len(goal) for val in z)
