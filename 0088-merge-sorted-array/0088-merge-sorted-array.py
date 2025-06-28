8ed8a70f8e843ed2a209982003d2eb064219e1b1
Shushant Rishav <54536673+shushantrishav@users.noreply.github.com>
2025-06-28 14:35:19 +0530

    Time: 0 ms (100%), Space: 18 MB (25.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,24 +1,23 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-            Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        p1 = m - 1  # pointer for nums1
-        p2 = n - 1  # pointer for nums2
-        p = m + n - 1  # pointer for placement in nums1
+        ptr_nums1 = m - 1  # pointer for nums1
+        ptr_nums2 = n - 1  # pointer for nums2
+        placement_idx = m + n - 1  # pointer for ele placement in nums1
 
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
+        while ptr_nums1 >= 0 and ptr_nums2 >= 0:
+            if nums1[ptr_nums1] > nums2[ptr_nums2]:
+                nums1[placement_idx] = nums1[ptr_nums1]
+                ptr_nums1 -= 1
             else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
+                nums1[placement_idx] = nums2[ptr_nums2]
+                ptr_nums2 -= 1
+            placement_idx -= 1 # move placement_idx to -1 position
 
         # If there are remaining elements in nums2, copy them
-        while p2 >= 0:
-            nums1[p] = nums2[p2]
-            p2 -= 1
-            p -= 1
-
+        while ptr_nums2 >= 0:
+            nums1[placement_idx] = nums2[ptr_nums2]
+            ptr_nums2 -= 1
+            placement_idx -= 1
